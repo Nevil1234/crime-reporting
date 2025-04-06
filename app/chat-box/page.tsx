ee3db2d47cb3885754a64a9e1ce8d1eeb06a6c79
Nevil1234 <122464295+Nevil1234@users.noreply.github.com>
2025-04-06 07:26:02 +0530

    commente some code

--- app/chat-box/page.tsx
+++ app/chat-box/page.tsx
@@ -1,11 +1,11 @@
-// filepath: app/chat-box/page.tsx
-"use client";
-import ChatComponent from "@/components/ComplainantChat";
+// // filepath: app/chat-box/page.tsx
+// "use client";
+// import ChatComponent from "@/components/ComplainantChat";
 
-export default function ChatBoxPage() {
-  return (
-    <main className="min-h-screen bg-gray-50 p-4">
-      <ChatComponent />
-    </main>
-  );
-}
+// export default function ChatBoxPage() {
+//   return (
+//     <main className="min-h-screen bg-gray-50 p-4">
+//       <ChatComponent />
+//     </main>
+//   );
+// }
